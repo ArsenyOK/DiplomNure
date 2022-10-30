7786e3a06e747211d47911c3b89ae2cfd5b9f904
ArsenyOK <arsen.pilipenko2014@gmail.com>
2022-10-30 20:19:24 +0200

    Add img router for uploading picture

---
+++ models/Image.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ImageSchema = new Schema(
+    {
+        name: {type: String, required: true, max: 100},
+        img: {data: Buffer, contentType: String}
+    }
+);
+
+module.exports = mongoose.model("Image", ImageSchema);
