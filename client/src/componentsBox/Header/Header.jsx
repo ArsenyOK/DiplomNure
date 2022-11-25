6ffbb7485b8f2bf14c2065e95a96896822126490
ArsenyOK <arsen.pilipenko2014@gmail.com>
2022-11-25 12:23:07 +0200

    Add eslint and prettier

--- client/src/componentsBox/Header/Header.jsx
+++ client/src/componentsBox/Header/Header.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
-import { HeaderBox } from '../styled-components/Header.styled';
-import AuthLinks from './UserLinks/AuthLinks';
-import GuestLinks from './UserLinks/GuestLinks';
+import React from "react";
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { HeaderBox } from "../styled-components/Header.styled";
+import AuthLinks from "./UserLinks/AuthLinks";
+import GuestLinks from "./UserLinks/GuestLinks";
 
 // const drawerWidth = 240;
 // const navItems = ['Home', 'About', 'Contact', 'CategoryBox'];
 
 const Header = (props) => {
-    const { isAuthenticated } = useSelector(store => store.auth)
-    return (
-        <HeaderBox>
-            <div className="header-logo">
-                <NavLink to="/">Logo</NavLink>
-            </div>
-            { !isAuthenticated ? <GuestLinks /> : <AuthLinks />}
-        </HeaderBox>
-    )
-}
+  const { isAuthenticated } = useSelector((store) => store.auth);
+  return (
+    <HeaderBox>
+      <div className="header-logo">
+        <NavLink to="/">Logo</NavLink>
+      </div>
+      {!isAuthenticated ? <GuestLinks /> : <AuthLinks />}
+    </HeaderBox>
+  );
+};
 export default Header;
--- client/src/componentsBox/RecipesPage/RecipeInfoPage/RecipePage.jsx
+++ client/src/componentsBox/RecipesPage/RecipeInfoPage/RecipePage.jsx
@@ -1,55 +1,51 @@
-import React, { useEffect, useCallback } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import Recipe from '../../../Components/Recipe/Recipe';
-import { getCurrentRecipe } from '../../../store/actions/itemAction';
+import React, { useEffect, useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import Recipe from "../../../Components/Recipe/Recipe";
+import { getCurrentRecipe } from "../../../store/actions/itemAction";
 
 const RecipePage = () => {
-    const params = useParams();
-    const { currentRecipe, loading } = useSelector((store) => store.recipes);
-    const dispatch = useDispatch();
+  const params = useParams();
+  const { currentRecipe, loading } = useSelector((store) => store.recipes);
+  const dispatch = useDispatch();
 
-    const onChangeRecipe = (recipeId) => {
-        dispatch(getCurrentRecipe(recipeId));
+  const onChangeRecipe = (recipeId) => {
+    dispatch(getCurrentRecipe(recipeId));
+  };
+
+  const refreshRecipe = () => {
+    window.scrollTo(0, 0);
+    if (params.id !== undefined) {
+      let id = params.id;
+
+      onChangeRecipe(id);
+      dispatch(getCurrentRecipe(id));
     }
+  };
 
-    const refreshRecipe = () => {
-        window.scrollTo(0, 0);
-        if (params.id !== undefined) {
-            let id = params.id;
-
-            onChangeRecipe(id);
-            dispatch(getCurrentRecipe(id));
-        }
+  const updateRefreshRecipe = useCallback(() => {
+    if (params.id !== undefined) {
+      refreshRecipe();
     }
+  }, [params.id]);
 
-    const updateRefreshRecipe = useCallback(() => {
-        if (params.id !== undefined) {
-            refreshRecipe();
-        }
-    }, [params.id])
+  useEffect(() => {
+    refreshRecipe();
+  }, []);
 
-    useEffect(() => {
-        refreshRecipe();
-    }, [])
+  useEffect(() => {
+    updateRefreshRecipe();
+  }, [updateRefreshRecipe]);
 
-    useEffect(() => {
-        updateRefreshRecipe();
-    }, [updateRefreshRecipe])
+  if (params.id !== undefined && !currentRecipe) {
+    return <>Loading...</>;
+  }
 
-    if (params.id !== undefined && !currentRecipe) {
-        return <>Loading...</>
-    }
+  console.log(currentRecipe, "currentRecipe");
 
-    console.log(currentRecipe, 'currentRecipe')
+  return (
+    <Recipe idRecipe={params.id} newContent={currentRecipe} loading={loading} />
+  );
+};
 
-    return (
-        <Recipe
-            idRecipe={params.id}
-            newContent={currentRecipe}
-            loading={loading}
-        />
-    )
-}
-
-export default RecipePage
+export default RecipePage;
